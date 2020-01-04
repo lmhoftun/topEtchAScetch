45474be6d2e5b573fd7facd51fffa2c0827ca245
lmhoftun <lmlindbo@gmail.com>
2020-01-04 12:25:10 +0100

    more on the base structure

---
+++ script.js
@@ -0,0 +1,20 @@
+/*These are but prliminary thoughts
+
+//function createGrid 
+    //init with 16x16
+    //from user input
+
+
+//function color
+    //draw with random colors
+
+//function black
+    //draw with black
+
+//function grayscale
+    //draw with 10% grayscale. 
+    //darken on overlap
+
+//function clearGrid
+    //clear everything
+    //keep colorselection
