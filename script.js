f9129dc36af24a72e12ca998eb2b1d73aba87298
lmhoftun <lmlindbo@gmail.com>
2020-01-04 12:44:57 +0100

    playing with js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*These are but prliminary thoughts
+/*These are but prliminary thoughts*/
 
 //function createGrid 
     //init with 16x16
@@ -18,3 +18,18 @@
 //function clearGrid
     //clear everything
     //keep colorselection
+
+/**************************** 
+ * Get buttons. Not sure if this is the way, yet.
+ * Some listeners perhaps?
+ *  <button id="btnClear">Clear</button>
+                <button id="btnResize">Resize</button>
+                <button id="btnBlack">Black</button>
+                <button id="btnColor">Color</button>
+                <button id="btnDarken">Darken</button>*/
+
+const gridContainer = document.getElementById('container');
+const btnClear = document.getElementById('btnClear');
+const btnBlack = document.getElementById('btnBlack');
+const btnColor = document.getElementById('btnColor');
+const btnDarken = document.getElementById('btnDarken');
